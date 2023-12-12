516674d85c4b5519e8be1c8ca3ed656816583d60
hekimapro <taprotec@gmail.com>
2023-12-12 08:12:17 +0300

    new changes

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
-// dependencies
-import cors from "cors";
-import { Socket } from "socket.io"
+// Importing dependencies
+import cors from "cors"
+import cluster from "cluster"
 import express, { Application } from "express"
-import { io, router, helpers, startServer, useClustering } from "bapig"
+import { router, helpers, useClustering, mongoose } from "bapig"
 
+// Initializing the Express application
+const application: Application = express()
 
-// initializing express
-const application: Application = express();
+// Security-related middleware
+application.disable("x-powered-by") // Disable the X-Powered-By header for security
+application.use(cors({ origin: "*" })) // Enable Cross-Origin Resource Sharing (CORS)
 
-// express middleware
-application.disable("x-powered-by");
-application.use(cors({ origin: "*" }));
-application.use(express.json({ limit: "100mb" }));
-application.use(require("express-fileupload")())
-application.use(express.static(helpers.staticFilesDirectory));
+// Parsing middleware
+application.use(express.json({ limit: "100mb" })) // Parse JSON requests with a limit of 100mb
 
+// Serving static files middleware
+application.use(express.static(helpers.staticFilesDirectory)) // Serve static files
 
-// API routes
-application.use("/api", router);
+// File upload middleware
+application.use(require("express-fileupload")()) // Enable file uploads
 
-// starting server withouth clustering
-// startServer(application);
+// ---------- End of Middleware Arrangement ----------
 
-// starting server with clustering
-useClustering(application)
+// API Endpoints
+application.use("/api", router) // Mount API routes
 
-// socket io
-io?.on("connection", (socket: Socket) => {
-    socket.on("message", (message: any) => {
-        console.log(message);
-    });
-});
+// Database Connection Event
+// Listening for the "connected" event on the Mongoose connection
+mongoose.connection.on("connected", () => {
+
+    // Requiring all database models once the connection is established
+    require("./database/collection")
+
+    // Running specific tasks on the first worker
+    const { worker } = cluster
+    if (worker && worker.id === 1) {
+
+        // Running specific tasks on the first worker only
+
+    }
+
+})
+
+// Server Starting and Clustering
+useClustering(application) // Utilize clustering for better performance and resilience
